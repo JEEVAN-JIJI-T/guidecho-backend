2f39cb59b3dc0f5646a9f1961f3f2b75449b2445
JEEVAN-JIJI-T <jeevanjijithomas.14@gmail.com>
2025-05-11 14:00:27 +0530

    Delete models directory

--- models/yolo_model.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', trust_repo=True)
-
-def detect_objects(img):
-    results = model(img)
-    labels = results.pandas().xyxy[0]['name'].tolist()
-    return ', '.join(set(labels)) if labels else 'No obstacles detected'
